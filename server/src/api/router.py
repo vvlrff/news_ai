f32a9645c712c387a4737e8c0db95a7dde7ad18a
Butte-bit <g.jarkovskij@yandex.ru>
2023-09-17 16:45:08 +0300

    [загрузка и отгрузка ] отгрузка: str (полное название файла)

---
+++ server/src/api/router.py
@@ -0,0 +1,34 @@
+import csv
+import json
+import os
+import random as rnd
+import re
+import sys
+
+from fastapi import APIRouter, Body, Depends, File, Query, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import and_, distinct, insert, select,  func
+
+
+
+router = APIRouter (
+    prefix='/api',
+    tags= ['api']
+)
+
+
+@router.post("/zagruzka")
+async def upload_file(file: UploadFile):
+    folder_path = os.getcwd() + r'\src\api\INPUT_\\'  # путь к папке, в которую нужно сохранить файл
+    file_path = os.path.join(folder_path, file.filename)  # объединяем путь к папке и имени файла
+    with open(file_path, "wb") as f:  # открываем файл на запись
+        f.write(await file.read())  # записываем содержимое загруженного файла в созданный файл
+    return JSONResponse(content=[])
+
+@router.post("/otgruzka")
+async def upload_file(file: str):
+    folder_path = os.getcwd() + r'\src\api\INPUT_\\'
+    file_path = os.path.join(folder_path, file)
+
+    return FileResponse(path=file_path,filename=file, media_type='multipart/form-data' )
---
+++ server/src/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .api.router import router as router_orm
+
+app = FastAPI(
+    title="HACK_16_20"
+)
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router_orm)
