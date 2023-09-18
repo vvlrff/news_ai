a5a92860849914e9ea719785690e57cc82a2710f
unhaten <csgold2002@mail.ru>
2023-09-19 01:15:34 +0300

    result page css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import {
+    Route,
+    createBrowserRouter,
+    RouterProvider,
+    createRoutesFromElements,
+} from "react-router-dom";
 import Layout from "./components/Layout";
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
-import ResultPage from './pages/ResultPage';
-import FileUploadPage from './pages/FileUploadPage';
-import CategoryPage from './pages/CategoryPage';
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ResultPage from "./pages/ResultPage";
+import FileUploadPage from "./pages/FileUploadPage";
+import CategoryPage from "./pages/CategoryPage";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />} >
-    <Route index element={<HomePage />} />
-    <Route path='upload' element={<FileUploadPage />} />
-    <Route path='result' element={<ResultPage />} />
-    <Route path='result/:category' element={<CategoryPage />} />
-    <Route path='*' element={<NotFoundPage />} />
-  </Route>
-))
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="upload" element={<FileUploadPage />} />
+            <Route path="result" element={<ResultPage />} />
+            <Route path="result/:category" element={<CategoryPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Route>
+    )
+);
 
 function App() {
-  return (
-      <RouterProvider router={router} />
-  )
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/pages/ResultPage/index.jsx
+++ frontend/src/pages/ResultPage/index.jsx
@@ -1,33 +1,38 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import s from "./ResultPage.module.scss"
-
+import s from "./ResultPage.module.scss";
 
 const ResultPage = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { state } = location;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { state } = location;
 
-  const submitData = (categoryData) => {
-    navigate(`/result/${categoryData.category}`, { state: categoryData });
-  };
+    const submitData = (categoryData) => {
+        navigate(`/result/${categoryData.category}`, { state: categoryData });
+    };
 
-  return (
-    <div className={s.container}>
-      ResultPage
-      {
-        Object.entries(state.response[0])
-          .map(([key, value]) => (
-            <>
-              <button onClick={() => submitData({ category: key, data: value })} key={key}>
-              Категория: {key}
-            </button>
-            </>
-          ))
-      }
-    </div>
-  )
-}
+    return (
+        <section>
+            <div className={s.container}>
+                <h2 className={s.header}>Выберите Категорию</h2>
+                <ul className={s.list}>
+                    {Object.entries(state.response[0]).map(([key, value]) => (
+                        <>
+                            <li
+                                className={s.card}
+                                onClick={() =>
+                                    submitData({ category: key, data: value })
+                                }
+                                key={key}
+                            >
+                                <p>{key}</p>
+                                {/* <img className={s.img} src="" alt="bgimg" /> */}
+                            </li>
+                        </>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+};
 
-export default ResultPage
-
-
+export default ResultPage;
