553010b001e0a9a0e5c3442816e30c8852a68d1d
Chief-tain <125179070+Chief-tain@users.noreply.github.com>
2023-09-18 13:48:19 +0300

    some stuff2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Clussifier.bert_classifier import  BertClassifier
 import pandas as pd
+import matplotlib.pyplot as plt
+from fuzzywuzzy import fuzz
 
 class Clussifier():
     def __init__(self):
@@ -17,14 +19,60 @@ class Clussifier():
                         9: 'Развлечения',
                         10: 'Общее'}
         
+        self.collector = None
+        
     def parse_xlsx(self, path):
         data = pd.read_excel(path, names=['text', 'channel_id'])  
-        return data['text'].tolist()
+        texts = data['text'].tolist()
+        indexes = data['channel_id'].tolist()
+        self.collector = dict(zip(texts, indexes))
+        return texts
     
     def crate_xlsx(self, dictionary):
         df = pd.DataFrame(dictionary)
-        df = df.transpose()
-        df.to_excel("output.xlsx") 
+        df = df.transpose().reset_index()
+        df.rename(columns={'index':'Название категории'}, inplace=True )
+        df = df[['Название категории', 'Общее количество', 'Количество без дубликтов']]
+
+        with pd.ExcelWriter('NaturaLP_ANSWER.xlsx', engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name='Статистика')
+            sheet = writer.sheets['Статистика']
+            sheet.set_column('B:D', 30)
+
+            plt.pie(df['Количество без дубликтов'], labels=df['Название категории'], radius=1.0)
+            plt.title('Количество сообщений без дубликатов по категориям')
+            plt.savefig('pie2.jpeg', dpi=200, bbox_inches='tight')
+            sheet.insert_image('F22', 'pie2.jpeg')
+            plt.close()
+
+            plt.pie(df['Общее количество'], labels=df['Название категории'], radius=1.0)
+            plt.title('Общее количество сообщений по категориям')
+            plt.savefig('pie1.jpeg', dpi=200, bbox_inches='tight')
+            sheet.insert_image('F2', 'pie1.jpeg')
+            plt.close()
+
+            for key, value in dictionary.items():
+                idshki = []
+                for el in value['Сообщения без дубликтов']:
+                    idshki.append(self.collector[el])
+                new_df = pd.DataFrame(list(zip(idshki, value['Сообщения без дубликтов'])), columns =['ID канала', 'Сообщения без дубликтов'])
+                new_df.to_excel(writer, sheet_name=key)
+                sheet = writer.sheets[key]
+                sheet.set_column('D:E', 300)
+    
+    def drop_dublikates(self, target, param):
+        cleaned_target = set(target)
+
+        for i in range(len(target)-1):
+            for j in range(i+1, len(target)):
+                if fuzz.WRatio(target[i], target[j]) > param:
+                    if len(target[i]) >= len(target[j]):
+                        cleaned_target.discard(target[j])
+                    else:
+                        cleaned_target.discard(target[i])
+
+        return list(cleaned_target)
+    
 
     def main(self, target):
         answer = {'Спорт': {'Общее количество': 0, 'Количество без дубликтов': 0, 'Сообщения': []},
@@ -42,8 +90,13 @@ class Clussifier():
         for element in target:
             el_cat = self.cat_name[self.clussifier.predict(element)]
             answer[el_cat]['Общее количество'] += 1
-            answer[el_cat]['Количество без дубликтов'] += 1
             answer[el_cat]['Сообщения'].append(element)
+
+        for value in answer.values():
+            value['Сообщения без дубликтов'] = self.drop_dublikates(value['Сообщения'], param=80)
+            value['Количество без дубликтов'] = len(value['Сообщения без дубликтов'])
+
+        print(answer)
 
         return answer
     
