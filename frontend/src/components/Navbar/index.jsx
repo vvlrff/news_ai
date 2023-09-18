a6b6fef37151358b16f5915761495c8bb01cce67
unhaten <csgold2002@mail.ru>
2023-09-18 03:51:19 +0300

    nakidal

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import s from "./Navbar.module.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import s from "./Navbar.module.scss";
 
 const Navbar = () => {
     return (
         <>
-            <Link to='/'>
-                HomePage
-            </Link>
-            <Link to='/data'>
-                DataPage
-            </Link>
+            <nav className={s.nav}>
+                <div className={s.container}>
+                    <div className={s.left}></div>
+                    <div className={s.linkContainer}>
+                        <Link to="/">HomePage</Link>
+                        <Link to="/data">DataPage</Link>
+                    </div>
+                </div>
+            </nav>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
