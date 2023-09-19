3516426c3091c21c50a236d88c5875218cf08ced
Butte-bit <g.jarkovskij@yandex.ru>
2023-09-19 20:32:18 +0300

    []

--- server/src/api/router.py
+++ server/src/api/router.py
@@ -10,7 +10,7 @@ from fastapi.responses import FileResponse, JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import and_, distinct, insert, select,  func
 
-
+from .nlp_men import Clussifier
 
 router = APIRouter (
     prefix='/api',
@@ -24,8 +24,10 @@ async def upload_file(file: UploadFile):
     file_path = os.path.join(folder_path, file.filename)  # объединяем путь к папке и имени файла
     with open(file_path, "wb") as f:  # открываем файл на запись
         f.write(await file.read())  # записываем содержимое загруженного файла в созданный файл
-    return JSONResponse(content=[])
-
+    test = Clussifier()
+    data = test.crate_xlsx(test.main(test.parse_xlsx(file_path)))
+    data_out = test.main(test.parse_xlsx(file_path))
+    return JSONResponse(content=data_out)
 
 @router.post("/vigruzka")
 async def upload_file(file: str):
@@ -33,3 +35,13 @@ async def upload_file(file: str):
     file_path = os.path.join(folder_path, file)  # объединяем путь к папке и имени файла
 
     return FileResponse(path=file_path,filename=file, media_type='multipart/form-data' )
+
+
+
+# @router.get("/answer_to_draw")
+# async def upload_file(path_to_input_data: str):
+#     test = Clussifier()
+#     folder_path = os.getcwd() + r'\src\api\INPUT_\\'+path_to_input_data
+#     test.crate_xlsx(test.main(test.parse_xlsx(folder_path)))
+
+#     return FileResponse(path=file_path,filename=file, media_type='multipart/form-data' )
