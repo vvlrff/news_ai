5f242092a0fcbe0a4c07d35d66277696a9af1d96
Chief-tain <125179070+Chief-tain@users.noreply.github.com>
2023-09-21 01:22:26 +0300

    changed_model_to_VK

---
+++ NLP_FORCE/bert_classifier.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+from transformers import BertTokenizer
+
+class BertClassifier:
+
+    def __init__(self, model_path, tokenizer_path):
+        self.tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = torch.load(model_path, map_location=self.device)
+        self.max_len = 512
+        self.out_features = self.model.bert.encoder.layer[1].output.dense.out_features
+        self.model.to(self.device)
+    
+    def predict(self, text):
+        sig = nn.Sigmoid()
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            truncation=True,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+        
+        out = {
+              'text': text,
+              'input_ids': encoding['input_ids'].flatten(),
+              'attention_mask': encoding['attention_mask'].flatten()
+          }
+        
+        input_ids = out["input_ids"].to(self.device)
+        attention_mask = out["attention_mask"].to(self.device)
+        
+        outputs = self.model(
+            input_ids=input_ids.unsqueeze(0),
+            attention_mask=attention_mask.unsqueeze(0)
+        )
+        
+        prediction = torch.argmax(outputs.logits, dim=1).cpu().numpy()[0]
+        probability = sig(outputs.logits).detach().numpy()[0][prediction]
+
+        if probability < 0.5:
+            prediction = 29
+        # print(prediction)
+        return prediction
