f9a40ae33fbf2b595c395ad772dc6e103e946321
Chief-tain <125179070+Chief-tain@users.noreply.github.com>
2023-09-21 13:45:28 +0300

    data2000

--- NLP_FORCE/vk.py
+++ NLP_FORCE/vk.py
@@ -1,4 +1,4 @@
-import vk_api, re
+import vk_api, re, emoji
 from fuzzywuzzy import fuzz
 
 vk_session = vk_api.VkApi(token='542ccee1542ccee1542ccee1a657397c855542c542ccee13127ef5f6ed1cb2d9bd69637')
@@ -6,34 +6,34 @@ vk_session = vk_api.VkApi(token='542ccee1542ccee1542ccee1a657397c855542c542ccee1
 vk = vk_session.get_api()
 
 cat_name = {
-            # 'Блоги.txt': '#блог',
-            # 'Новости и СМИ.txt': '#Новости',
-            # 'Развлечения и юмор.txt': '#Юмор',
-            # 'Технологии.txt': '#Технологии',
-            # 'Экономика.txt': '#Экономика',
-            # 'Бизнес и стартапы.txt': '#Стартап',
-            # 'Криптовалюты.txt': '#Криптовалюта',
-            # 'Путешествия.txt': '#Путешествия',
-            # 'Маркетинг, PR, реклама.txt': '#Маркетинг',
-            # 'Психология.txt': '#Психология',
-            # 'Дизайн.txt': '#Дизайн',
-            # 'Политика.txt': '#Политика',
-            # 'Искусство.txt': '#Искусство',
-            # 'Право.txt': '#Право',
-            # 'Образование и познавательное.txt': '#Образование',
-            # 'Спорт.txt': '#Спорт',
-            # 'Мода и красота.txt': '#Мода',
-            # 'Здоровье и медицина.txt': '#Здоровье #медицина',
-            # 'Картинки и фото.txt': '#фото',
-            'Софт и приложения.txt': '#программирование',
-            # 'Видео и фильмы.txt': '#фильмы',
-            # 'Музыка.txt': '#Музыка',
-            # 'Игры.txt': '#Игры',
-            # 'Еда и кулинария.txt': '#Еда #Кулинария',
-            # 'Цитаты.txt': '#Цитаты',
-            # 'Рукоделие.txt': '#Рукоделие',
-            # 'Финансы.txt': '#Финансы',
-            # 'Шоубиз.txt': '#Шоубиз'
+#             'Блоги.txt': '#блог',
+#             'Новости и СМИ.txt': '#Новости',
+#             'Развлечения и юмор.txt': '#Юмор',
+#             'Технологии.txt': '#Технологии',
+#             'Экономика.txt': '#Экономика',
+#             'Бизнес и стартапы.txt': '#Стартап',
+#             'Криптовалюты.txt': '#Криптовалюта',
+#             'Путешествия.txt': '#Путешествия',
+#             'Маркетинг, PR, реклама.txt': '#Маркетинг',
+#             'Психология.txt': '#Психология',
+#             'Дизайн.txt': '#Дизайн',
+#             'Политика.txt': '#Политика',
+#             'Искусство.txt': '#Искусство',
+#             'Право.txt': '#Право',
+#             'Образование и познавательное.txt': '#Образование',
+#             'Спорт.txt': '#Спорт',
+#             'Мода и красота.txt': '#Мода',
+#             'Здоровье и медицина.txt': '#Здоровье #медицина',
+#             'Картинки и фото.txt': '#фото',
+#             'Софт и приложения.txt': '#программирование',
+#             'Видео и фильмы.txt': '#фильмы',
+#             'Музыка.txt': '#Музыка',
+#             'Игры.txt': '#Игры',
+#             'Еда и кулинария.txt': '#Еда #Кулинария',
+#             'Цитаты.txt': '#Цитаты',
+#             'Рукоделие.txt': '#Рукоделие',
+#             'Финансы.txt': '#Финансы',
+            'Шоубиз.txt': '#Шоубиз'
             }
 
 def drop_dublikates(target, param):
@@ -55,12 +55,14 @@ def remove_links_and_hashtags(text):
     text_without_links = re.sub(r'http\S+|www.\S+', '', text)
     text_without_tags = re.sub(r'#\w+', '', text_without_links)
     text_without_n = re.sub(r'\n', '. ', text_without_tags)
+    text_without_smileys = emoji.replace_emoji(text_without_n)
+    text_without_smileys_and_space = re.sub(' \.', '', text_without_smileys)
     
-    return text_without_n
+    return text_without_smileys_and_space
 
 
 for key, value in cat_name.items():
-    with open(f'NLP_FORCE\ForTrain2\{key}', mode='w', encoding='utf-8') as file:
+    with open(f'NLP_FORCE\ForTrain4\{key}', mode='w', encoding='utf-8') as file:
         if key == 'Картинки и фото.txt':
             min_len = 70
         elif key == 'Цитаты.txt':
@@ -69,7 +71,7 @@ for key, value in cat_name.items():
             min_len = 120
         total_data = []
         time_now = 1695236288
-        while len(total_data) < 1000:
+        while len(total_data) < 2000:
             search_results = vk.newsfeed.search(q=value, count=200, extended=0, end_time = time_now, start_time = time_now - 60 * 60 * 24 * 7)
             current_data_list = []
             for item in search_results['items']:
