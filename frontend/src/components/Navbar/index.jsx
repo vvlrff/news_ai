e263dcc59cb74a8d03c59ee353838237dadf7250
unhaten <csgold2002@mail.ru>
2023-09-21 19:56:13 +0300

    navedit

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -7,7 +7,13 @@ const Navbar = () => {
         <>
             <nav className={s.nav}>
                 <div className={s.container}>
-                    <div className={s.left}></div>
+                    <div className={s.left}>
+                        <p className={s.text}>NaturaLP</p>
+                        <img
+                            src="https://thumb.tildacdn.com/tild3237-3232-4266-a362-333130353936/-/resize/104x/-/format/webp/_____2__1.png"
+                            alt=""
+                        />
+                    </div>
                     <div className={s.linkContainer}>
                         <Link to="/">На главную</Link>
                         <Link to="/upload">Загрузить файл</Link>
